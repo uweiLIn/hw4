1fba85223951f986142f89cd7c2b558444e292ed
uweiLIn <Lin33113311@gmail.com>
2021-06-12 15:16:50 +0800

    update Q2

--- hw4_2/main.cpp
+++ hw4_2/main.cpp
@@ -12,9 +12,10 @@ DigitalIn encoder(D11);
 
 BBCar car(pin5, pin6, servo_ticker);
 
-//char value;
+char value;
 
 float factor = 0;
+int mode = 0;
 
 int main(){
    uart.set_baud(9600);
@@ -23,17 +24,24 @@ int main(){
       if(uart.readable()){
             char recv[1];
             uart.read(recv, sizeof(recv));
-            //value = *recv;
+            value = *recv;
             //printf("%c ",x)
-            if (recv[0] == 0) {
-                printf("no suittable line");
-                car.goStraight(50);
-            } else if (recv[0] == 1) {
-                factor = recv[1] / 100.0;
+            if (mode) {
+                factor = value / 100.0;
                 if (factor < 0.1 && factor > -0.1) {
                     factor = 0;
-                }
+                } 
+                // give open MV an allowable detect error so that car wont 
+                // swing from side to side 
                 car.turn(50, factor);
+
+            }
+            if (value == 0) {
+                mode = 0;
+                printf("no suittable line");
+                car.goStraight(50);
+            } else if (value == 1) {
+                mode = 1;
             }
       }
    }
